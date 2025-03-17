2c8a96d4dfdca7c4a7a4d5e631d52ec30ed2762f
David Runger <davidjrunger@gmail.com>
2025-03-17 13:58:30 -0500

    Avoid N+1 queries coming from #version_limit when destroying records
    Fixes #1510

--- spec/dummy_app/app/versions/post_version.rb
+++ spec/dummy_app/app/versions/post_version.rb
@@ -2,4 +2,20 @@
 
 class PostVersion < PaperTrail::Version
   self.table_name = "post_versions"
+
+  # Hack to simulate a version class that doesn't have the optional `item_subtype` column. BEGIN >>>
+  def item_subtype
+    raise "This method should not be called!"
+  end
+
+  # rubocop:disable Style/OptionalBooleanParameter
+  def respond_to?(method_name, include_private = false)
+    # rubocop:enable Style/OptionalBooleanParameter
+    if method_name.to_sym == :item_subtype
+      false
+    else
+      super
+    end
+  end
+  # <<< END Hack to simulate a version class that doesn't have the optional `item_subtype` column.
 end
